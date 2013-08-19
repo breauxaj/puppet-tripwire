61e77733b9439f8d01d73430ba6cbdc15cd8afac
Adam Breaux <breauxaj@gmail.com>
2013-08-19 10:56:16 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+Tripwire
+========
+
+Tripwire is a free software security and data integrity tool useful for
+monitoring and alerting on specific file change(s) on a range of systems.
+
+Do not use the passphrases below, create your own.
+
+Samples
+=======
+
+include tripwire
+
+tripwire::config { 'default':
+  localpassphrase => '8Ls1L6o0J4dZx3v',
+  sitepassphrase  => 'X6L4K4w72L7j42t',
+  globalemail     => 'admin@domain.com'
+}
---
+++ manifests/init.pp
@@ -0,0 +1,85 @@
+class tripwire {
+  $required = $operatingsystem ? {
+    /(?i-mx:centos|fedora|redhat|scientific)/ => 'tripwire',
+  }
+
+  $paths = $operatingsystem ? {
+    /(?i-mx:centos|fedora|redhat|scientific)/ => [ '/etc/tripwire', '/var/lib/tripwire' ],
+  }
+
+  package { $required: ensure => latest }
+
+  file { $paths:
+    ensure => directory,
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0700',
+  }
+
+}
+
+define tripwire::config ( $localpassphrase,
+                          $sitepassphrase,
+                          $globalemail ) {
+
+  file { '/etc/tripwire/Makefile':
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0440',
+    content => template('tripwire/Makefile.erb'),
+    require => Package['tripwire'],
+  }
+
+  file { '/etc/tripwire/twcfg.txt':
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0440',
+    content => template('tripwire/twcfg.erb'),
+    require => Package['tripwire'],
+  }
+
+  file { '/etc/tripwire/twpol.txt':
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0440',
+    content => template('tripwire/twpol.erb'),
+    require => Package['tripwire'],
+  }
+
+  file { '/etc/cron.daily/tripwire-check':
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0755',
+    source  => 'puppet:///modules/tripwire/tripwire-check',
+    require => Package['tripwire'],
+  }
+    
+  file { '/var/lib/tripwire/tripwire.twd':
+    owner => 'root',
+    group => 'root',
+    mode  => '0440',
+  }
+
+  exec { init-tripwire:
+    path    => '/bin:/usr/bin',
+    command => 'make -C /etc/tripwire',
+    cwd     => '/etc/tripwire',
+    creates => '/var/lib/tripwire/tripwire.twd',
+    timeout => 10000,
+    require => [
+      File['/etc/tripwire/Makefile'],
+      File['/etc/tripwire/twcfg.txt'],
+      File['/etc/tripwire/twpol.txt'],
+    ],
+  }
+    
+  exec { update-tripwire:
+    path        => '/sbin:/usr/sbin',
+    command     => "tripwire --update-policy -Z low --local-passphrase '$localpassphrase' --site-passphrase '$sitepassphrase' --quiet /etc/tripwire/twpol.txt",
+    cwd         => '/etc/tripwire',
+    timeout     => 10000,
+    refreshonly => 'true',
+    subscribe   => File['/etc/tripwire/twpol.txt'],
+  }
+
+}
